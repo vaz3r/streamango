8e13afa3b6ca951b0257672e561c030b5ad17730
vaz3r <vazer.dpr@gmail.com>
2019-06-26 01:40:57 +0100

    Init

---
+++ precache-manifest.c825fcd9de1bc15be50bc7f470d9d22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caf78798da4e56f733e66f706f4b266",
+    "url": "/streamango/index.html"
+  },
+  {
+    "revision": "b50f7f853bc8b9ce0c78",
+    "url": "/streamango/static/css/main.c33faec4.chunk.css"
+  },
+  {
+    "revision": "64bdf502206b16f9bf26",
+    "url": "/streamango/static/js/2.47e6c77d.chunk.js"
+  },
+  {
+    "revision": "b50f7f853bc8b9ce0c78",
+    "url": "/streamango/static/js/main.95d2c5fd.chunk.js"
+  },
+  {
+    "revision": "d51d705b4e64dea087fc",
+    "url": "/streamango/static/js/runtime~main.5dbee0cc.js"
+  },
+  {
+    "revision": "9c78962e574f4d77815e259cc79be58d",
+    "url": "/streamango/static/media/logo2.9c78962e.svg"
+  }
+]);
